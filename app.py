a0f4afee8bd220d3b0e7b5f2e03a2fa768c052b8
Admin <bisnuraj76@gmail.com>
2026-06-23 15:50:01 +0545

    Improved UI with navigation links

--- app.py
+++ app.py
@@ -2,17 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# This is HOME ROute
 
+# HOME ROUTE
 @app.route("/")
 def home():
-    return "<h1>Hello from Codespaces!</h1>"
+    return """
+    <h1>Welcome to My Mini Blog 🚀</h1>
+    <p>This is the home page running on Flask.</p>
+    <a href="/about">Go to About</a>
+    """
 
-# THis is About Route
 
+# ABOUT ROUTE
 @app.route("/about")
 def about():
-    return "<h2>This is the about page</h2>"
+    return """
+    <h2>About Page</h2>
+    <p>This Flask app is running from my PC and synced with GitHub.</p>
+    <a href="/">Back to Home</a>
+    """
+
 
 if __name__ == "__main__":
     app.run(debug=True)
